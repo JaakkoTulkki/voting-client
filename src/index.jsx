e56bae799e4d7c1a9f065dd0aaf8ff94544edba0
Jaakko Tulkki <jaakko.tulkki@sainsburys.co.uk>
2016-10-31 16:33:06 +0000

    tests passing

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
-console.log("alive");
 ReactDOM.render(
     <Voting pair={pair}/>,
     document.getElementById('app')
